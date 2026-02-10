5c923e6d59ad4e33a0732ed67735b1ab98b8cca9
sodatax <[200~62825932+sodatax@users.noreply.github.com~>
2026-02-10 14:46:51 -0800

    added to form and styles

---
+++ public/js/appointment.js
@@ -0,0 +1,26 @@
+document.getElementById("appointment-form").onsubmit = () => {
+
+    clearErrors();
+    let isValid = false;
+
+    let fname = document.getElementById("fname").value.trim();
+    if(!fname){
+        document.getElementById("err-fname").style.display = "block";
+        isValid = false;
+    }
+
+    let lname = document.getElementById("lname").value.trim();
+    if(!fname){
+        document.getElementById("err-lname").style.display = "block";
+        isValid = false;
+    }
+
+    return isValid;
+    }
+
+function clearErrors(){
+    let errors = document.getElementsByClassName("err");
+    for(let i=0; i<errors.length; i++){
+        errors[i].style.display = "none";
+    }
+}
