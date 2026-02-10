2ff1e5cc73d8d77b5986029237238a72e1c5b2ac
tonymythical <174657016+tonymythical@users.noreply.github.com>
2026-02-10 14:46:51 -0800

    setup routing

--- app.js
+++ app.js
@@ -8,8 +8,34 @@ app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+// app.get("/", (req, res) => {
+//   res.send("Hello, World!");
+// });
+
+
+const appointments = [];
+
+app.get('/', (req, res) => {
+  res.sendFile(`${import.meta.dirname}/views/home.html`);
+});
+
+app.get('/admin', (req, res) => {
+  res.send(appointments);
+});
+
+app.post('/submit-appointment', (req, res) => {
+  
+  const appointment = {
+    fname: req.body.err-fname,
+    lname: req.body.err-lname,
+    date: req.body.date,
+    time: req.body.time,
+    timestamp: new Date()
+  };
+  
+  appointments.push(appointment);
+  
+  res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
 });
 
 app.listen(PORT, () => {
